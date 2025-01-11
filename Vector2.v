62be5bbdd69a70cea55374c615a048eee5aea31b
asarkar8400 <74608503+asarkar8400@users.noreply.github.com>
2025-01-11 14:46:32 -0500

    Add files via upload

---
+++ Vector2.v
@@ -0,0 +1,14 @@
+//-----------------------------------------------------------------------------
+// Description : Solution to Vector2 on HDLBits
+//-----------------------------------------------------------------------------
+
+module top_module( 
+    input [31:0] in,
+    output [31:0] out );//
+
+    assign out[31:24] = in[7:0];
+    assign out[23:16] = in[15:8];
+    assign out[15:8]  = in[23:16];
+    assign out[7:0]   = in[31:24];
+
+endmodule
