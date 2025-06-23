cab433f39d395b455ad61000d494ff0f8ed5075d
Aritro Sarkar <74608503+asarkar8400@users.noreply.github.com>
2025-06-22 21:13:40 -0400

    Update Module_add.v

--- Module_add.v
+++ Module_add.v
@@ -3,22 +3,11 @@ module top_module(
     input [31:0] b,
     output [31:0] sum
 );
-    wire [15:0] a1, a2, b1, b2, sum1, sum2;
-    wire cin1, cout1, cin2, cout2;
+    wire cin1, cout1, cout2;
+    wire [15:0] sum1, sum2;
     
-    assign a2 = a[31:16];
-    assign a1 = a[15:0];
-    
-    assign b2 = b[31:16];
-    assign b1 = b[15:0];
-    
-    assign sum2 = sum[31:16];
-    assign sum1 = sum[15:0];
-    
-    assign cin2 = cout1;
-    
-    add16 lower(a1, b1, cin1, sum1, cout1);
-    add16 upper(a2, b2, cin2, sum2, cout2);
+    add16 instance1(.a(a[15:0]), .b(b[15:0]), .cin(cin1), .cout(cout1), .sum(sum1));
+    add16 instance2(.a(a[31:16]), .b(b[31:16]), .cin(cout1), .cout(cout2), .sum(sum2));
     
     assign sum = {sum2, sum1};
 
