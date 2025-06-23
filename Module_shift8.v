4c7a80992506546a4613b43297c18429eb61de49
Aritro Sarkar <74608503+asarkar8400@users.noreply.github.com>
2025-06-22 21:01:42 -0400

    Update Module_shift8.v

--- Module_shift8.v
+++ Module_shift8.v
@@ -4,23 +4,18 @@ module top_module (
     input [1:0] sel, 
     output [7:0] q 
 );
-    wire [7:0] d1, d2, d3;
     wire [7:0] q1, q2, q3;
     
-    assign d1 = d;
-    assign d2 = q1;
-    assign d3 = q2;
+    my_dff8 instance1(.clk(clk), .d(d), .q(q1));
+    my_dff8 instance2(.clk(clk), .d(q1), .q(q2));
+    my_dff8 instance3(.clk(clk), .d(q2), .q(q3));
     
-    my_dff8 instance1(clk, d1, q1);
-    my_dff8 instance2(clk, d2, q2);
-    my_dff8 instance3(clk, d3, q3);
-    
-    always@(*) begin
-        case (sel)
-            2'b00: q = d1;
-            2'b01: q = d2;
-            2'b10: q = d3;
-            2'b11: q = q3;
-        endcase 
-    end
+    always @ (d, q1, q2, q3, sel) begin
+        case(sel)
+            2'b00 : q = d;
+            2'b01 : q = q1;
+            2'b10 : q = q2;
+            default : q = q3;
+        endcase
+    end             
 endmodule
