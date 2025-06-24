a8bae5595ee624d103c50a9d939edf36cdd6476a
Aritro Sarkar <74608503+asarkar8400@users.noreply.github.com>
2025-06-23 23:06:31 -0400

    Update Module_cseladd.v

--- Module_cseladd.v
+++ Module_cseladd.v
@@ -3,20 +3,21 @@ module top_module(
     input [31:0] b,
     output [31:0] sum
 );
-    wire cin1, cin2, cin3;
+    wire cin1, cin2;
     wire cout1, cout2, cout3;
     wire [15:0] sum1, sum2, sum3;
     
     assign cin1 = 1'b0;
-    assign cin2 = 1'b0;
-    assign cin3 = 1'b1;
+    assign cin2 = 1'b1;
     
-    assign sum1 = sum[15:0];
-  
-    add16 lower(a[15:0], b[15:0], cin1, sum1, cout1);
-    add16 upper1(a[31:16], b[31:16], cin2, sum2, cout2);
-    add16 upper2(a[31:16], b[31:16], cin3, sum3, cout3);
+    add16 instance1(.a(a[15:0]), .b(b[15:0]), .cin(cin1), .sum(sum1), .cout(cout1));
+    add16 instance2(.a(a[31:16]), .b(b[31:16]), .cin(cin1), .sum(sum2), .cout(cout2));
+    add16 instance3(.a(a[31:16]), .b(b[31:16]), .cin(cin2), .sum(sum3), .cout(cout3));
     
-    assign sum = cout1 ? {sum3, sum1} : {sum2, sum1};
-    
+    always @ (*) begin
+        if(cout1 == 1)
+            sum = {sum3, sum1};
+        else
+            sum = {sum2, sum1};
+    end 
 endmodule
